1465940b374b938fc7934baa81f88724d5b7ae7d
utaha1228 <utaha1228@gmail.com>
2024-12-30 17:50:12 +0800

    change leaderboard to multi-message

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import tasks
-import hashlib
+import time
 
 import config
-from scrap import get_message
+from scrap import load_games, check_config
+from utils import *
 
 
 intents = discord.Intents.default()
@@ -13,29 +14,63 @@ bot = discord.Client(intents=intents)
 
 @bot.event
 async def on_ready():
+    check_config()
     print(f'We have logged in as {bot.user}')
-    channel = bot.get_channel(config.CHANNEL_ID)
 
-    msg = await channel.send(content="No game yet!")
+    # clear all message in channel
+    task.indv_channel = bot.get_channel(config.INDV_CHANNEL_ID)
+    task.team_channel = bot.get_channel(config.TEAM_CHANNEL_ID)
 
-    task.msg_id   = msg.id
-    task.prev_hsh = None
-    task.channel  = channel
+    async def clear_channel(channel):
+        async for message in channel.history(limit=100):
+            await message.delete()
+            time.sleep(1)
+
+    await clear_channel(task.indv_channel)
+    await clear_channel(task.team_channel)
+
+    task.indv_msg_ids = []
+    for i in range(4):
+        msg = await task.indv_channel.send(content="``` \n```")
+        task.indv_msg_ids.append(msg.id)
+
+    msg = await task.team_channel.send(content="``` \n```")
+    task.team_msg_id = msg.id
+
+    task.username2name = get_username2name_mapping()
+    name2team = get_username2team_mapping()
+
+    task.username2team = {}
+
+    for username, name in task.username2name.items():
+        if name not in name2team:
+            print(f"Hey {name}, you are not in the team list WTF")
+            continue
+        task.username2team[username] = name2team[name]
+
+    task.all_players   = list(task.username2name.keys())
+
     task.start()
 
 @tasks.loop(seconds=config.UPDATE_PERIOD)
 async def task():
-    new_msg = get_message()
-    new_hsh = hashlib.sha256(new_msg.encode()).hexdigest()
+    games = load_games()
+    indv_result = calculate_score(games, task.all_players, task.username2name)
+    team_result = calculate_score(games, task.all_players, task.username2team)
 
-    print(f"{task.prev_hsh}, {new_hsh}")
+    indv = format_leaderboard(indv_result)
+    # assert len(indv) == 4, f"number of messages don't match for indv leaderboard {len(indv)} != {len(task.indv_msg_ids)}" 
+    for i in range(len(indv)):
+        msg_id = task.indv_msg_ids[i]
+        msg = await task.indv_channel.fetch_message(msg_id)
+        await msg.edit(content=indv[i])
 
-    if task.prev_hsh == new_hsh:
-        return
 
-    task.prev_hsh = new_hsh
-    msg = await task.channel.fetch_message(task.msg_id)
-    await msg.edit(content=new_msg)
+    team = format_leaderboard(team_result)
+    assert len(team) == 1, str(len(team))
+    team_msg = team[0]
+    msg = await task.team_channel.fetch_message(task.team_msg_id)
+    await msg.edit(content=team_msg)
 
 
 bot.run(config.BOT_TOKEN)
---
+++ utils.py
@@ -0,0 +1,122 @@
+import config
+
+# Load csv file containting Majsoul username and real name
+def get_username2name_mapping():
+    mp = {}
+    have_seen = set({})
+    dups = set({})
+    with open("names.csv") as f:
+        f.readline()  # first line column names
+        for line in f:
+            _, first_name, last_name, username, _ = line.split(",")
+
+            # sanitize
+            first_name = first_name.strip()
+            last_name = last_name.strip()
+
+            mp[username] = (first_name, last_name)
+            if first_name in have_seen:
+                dups.add(first_name)
+            else:
+                have_seen.add(first_name)
+    ret = {}
+    for username, name in mp.items():
+        first, last = name
+        if first in dups:
+            ret[username] = first + last[0].upper()
+        else:
+            ret[username] = first  # unique first name
+    return ret
+
+def get_username2team_mapping():
+    mp = {}
+    with open("teams.csv") as f:
+        for line in f:
+            team_name, *members = line.split(",")
+            for member in members:
+                member = member.strip()
+                if len(member) == 0:  # empty string
+                    continue
+
+                mp[member] = team_name
+    return mp
+
+# `name_mapping` decides what name to show on leaderboard
+# In case of team leaderboard, it maps multiple people
+# to the same names (the team name).
+def calculate_score(games, all_players, name_mapping=None):
+    name2score = {}
+    name2rank  = {}
+
+    for player in all_players:
+        if name_mapping is not None:
+            if player not in name_mapping:
+                continue
+            player = name_mapping[player]
+
+        name2score[player] = 0
+        name2rank[player]  = [0, 0, 0, 0]
+
+    for game in games:
+        players = [None] * 4
+
+        for account in game["accounts"]:
+            name = account["nickname"]
+            seat = account["seat"]
+
+            if name_mapping is not None:
+                name = name_mapping.get(name, None)
+
+            players[seat] = name
+
+        scores = game["result"]["players"]
+        starting_score = sum([score["part_point_1"] for score in scores]) // 4
+        assert starting_score % 100 == 0
+
+        uma = config.uma[:]
+        for rank, score in enumerate(scores):
+            seat = score["seat"]
+            # store the actual score * 10 so it's an integer
+            delta = (score["part_point_1"] - starting_score) // 100
+            delta += uma[rank] * 10
+
+            name = players[seat]
+
+            if name is None:
+                continue
+
+            name2score[name] += delta
+            name2rank[name][rank] += 1
+
+    lines = []
+
+    for name in name2score:
+        score = name2score[name]
+        rank = name2rank[name]
+        lines.append((score, name, rank))
+
+    lines.sort(reverse=True)
+
+    return lines
+
+def format_leaderboard(lines):
+    message = f"""{"Rk":<4}{"Score":<8}{"1st":<4}{"2nd":<4}{"3rd":<4}{"4th":<4}{"name"}\n"""
+    for i, line in enumerate(lines, start=1):
+        score, name, rank = line
+
+        if score >= 0:
+            whole   = score // 10
+            decimal = abs(score) % 10
+        else:
+            whole = score // 10 + 1
+            decimal = abs(score) % 10
+        score = f"{whole}.{decimal}"
+
+        message += f"{i:<4}{score:<8}{rank[0]:<4}{rank[1]:<4}{rank[2]:<4}{rank[3]:<4}{name}\n"
+
+    # group per 40 lines
+    lines_per_msg = 40
+    message = message.split("\n")
+    message = ["\n".join(message[i:i+lines_per_msg]) for i in range(0, len(message), lines_per_msg)]
+    message = ["```\n" + s + "\n```\n"for s in message]
+    return message
