25408a8cb27235505c394e60ebcb2e36d27751fa
utaha1228 <utaha1228@gmail.com>
2025-05-05 16:43:57 -0400

    add sanma league

--- scrap.py
+++ scrap.py
@@ -10,12 +10,14 @@ def check_config():
     assert re.fullmatch(pattern, config.MS_TOKEN), "Majsoul token format: Majsoul XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
     assert isinstance(config.TOURN_ID, int), "Tournament ID should be an integer"
     assert isinstance(config.SEASON_ID, int), "Season ID should be an integer"
+    assert isinstance(config.SANMA_TOURN_ID, int), "Sanma Tournament ID should be an integer"
+    assert isinstance(config.SANMA_SEASON_ID, int), "Sanma Season ID should be an integer"
 
-def load_games_offset(offset, limit=100):
+def load_games_offset(tourn_id, season_id, offset, limit=100):
     url = f"https://engame.mahjongsoul.com/api/contest_gate/api/contest/fetch_contest_game_records"
     params = {
-        "unique_id": config.TOURN_ID,
-        "season_id": config.SEASON_ID,
+        "unique_id": tourn_id,
+        "season_id": season_id,
         "offset": offset,
         "limit": limit,
     }
@@ -28,12 +30,12 @@ def load_games_offset(offset, limit=100):
     data = r.json()
     return data["data"]["record_list"]
 
-def load_games():
+def load_games(tourn_id, season_id):
     games = []
     offset = 0
     step = 100  # get 100 games per request
     while True:
-        result = load_games_offset(offset, limit=step)
+        result = load_games_offset(tourn_id, season_id, offset, limit=step)
         if len(result) == 0:
             break
 
