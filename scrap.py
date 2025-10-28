f9b3c1186d25584305b9ca0d000992d5627d82ae
Redorangegamez <54161018+Redorangegamez@users.noreply.github.com>
2025-10-28 11:28:41 -0400

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,6 +1,4 @@
 import re
-import requests
-
 import config
 
 def check_config():
@@ -12,39 +10,3 @@ def check_config():
     assert isinstance(config.SEASON_ID, int), "Season ID should be an integer"
     assert isinstance(config.SANMA_TOURN_ID, int), "Sanma Tournament ID should be an integer"
     assert isinstance(config.SANMA_SEASON_ID, int), "Sanma Season ID should be an integer"
-
-def load_games_offset(tourn_id, season_id, offset, limit=100):
-    url = f"https://engame.mahjongsoul.com/api/contest_gate/api/contest/fetch_contest_game_records"
-    params = {
-        "unique_id": tourn_id,
-        "season_id": season_id,
-        "offset": offset,
-        "limit": limit,
-    }
-    headers = {
-        "Authorization": config.MS_TOKEN,
-    }
-    r = requests.get(url, params=params, headers=headers)
-
-    assert r.status_code == 200, f"""\
-Failed to fetch game records. Error code: {r.status_code}
-JSON = {r.json()}
-data = {r.text}
-"""
-
-    data = r.json()
-    return data["data"]["record_list"]
-
-def load_games(tourn_id, season_id):
-    games = []
-    offset = 0
-    step = 100  # get 100 games per request
-    while True:
-        result = load_games_offset(tourn_id, season_id, offset, limit=step)
-        if len(result) == 0:
-            break
-
-        games += result
-        offset += len(result)
-
-    return games
