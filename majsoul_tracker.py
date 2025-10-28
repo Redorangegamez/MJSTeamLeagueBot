292716d9370e25de9ac0c1ba206f64ca7bf5b304
Redorangegamez <54161018+Redorangegamez@users.noreply.github.com>
2025-10-28 12:09:39 -0400

    Update majsoul_tracker.py

--- majsoul_tracker.py
+++ majsoul_tracker.py
@@ -61,8 +61,6 @@ async def get_readied_players(lobby: int, season: int, players: int):
         return f"# **{players}** Player Lobby\n**Ready (0)**{playing_str}"
 
     ready_players.sort(key=lambda x: x.lower())
-    new_list = []
-    for i, player in enumerate(ready_players):
-        new_list[i] = f"{player} ({username2name_mapping.get(player, 'Unknown')})"
-    ready_str = ", ".join(new_list)
+    ready_players[:] = [f"{player} ({username2name_mapping.get(player, 'Unknown')})" for player in ready_players]
+    ready_str = ", ".join(ready_players)
     return f"# **{players}** Player Lobby\n**Ready ({len(ready_players)}):** {ready_str}{playing_str}"
