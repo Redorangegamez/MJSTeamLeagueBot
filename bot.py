f981a113db47e4b9efd1228676bceb70281ceafa
Redorangegamez <54161018+Redorangegamez@users.noreply.github.com>
2026-05-12 00:02:01 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,24 +60,25 @@ async def on_ready():
 
     # clear all messages in status channel
     status_loop.channel = bot.get_channel(config.STATUS_CHANNEL_ID)
+    print('here0')
     
     # clear all message in leaderboard channels
     leaderboard_loop.indv_channel = bot.get_channel(config.INDV_CHANNEL_ID)
     leaderboard_loop.team_channel = bot.get_channel(config.TEAM_CHANNEL_ID)
     leaderboard_loop.sanma_indv_channel = bot.get_channel(config.SANMA_INDV_CHANNEL_ID)
     leaderboard_loop.sanma_team_channel = bot.get_channel(config.SANMA_TEAM_CHANNEL_ID)
-    
+    print('fetched leaderboard channels')
     async def clear_channel(channel):
         async for message in channel.history(limit=100):
             await message.delete()
             time.sleep(1)
-
+    
     await clear_channel(leaderboard_loop.indv_channel)
     await clear_channel(leaderboard_loop.team_channel)
     await clear_channel(leaderboard_loop.sanma_indv_channel)
     await clear_channel(leaderboard_loop.sanma_team_channel)
     await clear_channel(status_loop.channel)
-
+    print('cleared channels')
     status_loop.status_msg_id = config.STATUS_CHANNEL_MSG_IDS
     print('here1')
     
